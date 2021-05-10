7aa49c14ed83f1afc3a487b9492832c564d4cae6
Renan Moreira <piumoreiras@gmail.com>
2021-05-10 09:05:37 -0300

    Spring Boot Actuator

--- src/main/java/br/com/inatel/jamming/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/inatel/jamming/config/security/SecurityConfigurations.java
@@ -51,6 +51,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.POST, "/users").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+//		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 		.anyRequest().authenticated()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
